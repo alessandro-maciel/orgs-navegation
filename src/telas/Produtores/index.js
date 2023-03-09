a9c520757f89e39f5ff5400772c5718066e74e2c
Alessandro Maciel <alessandromaciel237@gmail.com>
2023-03-08 21:35:43 -0300

    adicionando botão de finalização de compra

--- src/telas/Produtores/index.js
+++ src/telas/Produtores/index.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, Text, StyleSheet } from 'react-native';
 
 import Produtor from './componentes/Produtor';
 import Topo from './componentes/Topo';
 import useProdutores from '../../hooks/useProdutores';
 import useTextos from '../../hooks/useTextos';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 export default function Produtores({ melhoresProdutores }) {
-  const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
   const navigation = useNavigation();
+  const route = useRoute();
+
+  const [exibeMenssagem, setExibeMenssagem] = useState();
+
+  const lista = useProdutores(melhoresProdutores);
+  const { tituloProdutores, mensagemCompra } = useTextos();
+
+  const nomeCompra = route.params?.compra.nome;
+  const timestampCompra = route.params?.compra.timestamp;
+
+  const mensagemCompleta = mensagemCompra?.replace('$NOME', nomeCompra);
+
+  useEffect(()=>{
+    setExibeMenssagem(!!nomeCompra)
+    let timeout;
+
+    if (nomeCompra) {
+      timeout = setTimeout((()=> {
+        setExibeMenssagem(false)
+      }),3000);
+    }
+
+    return () => clearTimeout(timeout);
+
+  }, [timestampCompra])
 
   const TopoLista = () => {
     return <>
       <Topo melhoresProdutores={melhoresProdutores} />
+      { exibeMenssagem && <Text style={estilos.compra}>{ mensagemCompleta }</Text> }
       <Text style={estilos.titulo}>{tituloProdutores}</Text>
     </>
   }
@@ -40,5 +64,12 @@ const estilos = StyleSheet.create({
     marginTop: 16,
     fontWeight: 'bold',
     color: '#464646',
+  },
+  compra: {
+    backgroundColor: "#EAF5F3",
+    padding: 16,
+    color: "#464646",
+    fontSize: 16,
+    lineHeight: 26,
   }
 })
